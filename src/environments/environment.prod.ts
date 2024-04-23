57bcee96c5a5a4e685b3a791566f67b94e92163f
Azat <harutyunyanazat29@gmail.com>
2024-04-23 19:04:52 +0400

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+	production: true,
+	firebaseAPIKey: 'AIzaSyCpxYLe0ImFGg1KcwngAK-UUjU2V5z9R98'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+	production: false,
+	firebaseAPIKey: 'AIzaSyCpxYLe0ImFGg1KcwngAK-UUjU2V5z9R98'
+}
